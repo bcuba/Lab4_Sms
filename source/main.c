38602567c778aecb3db5441e98f00966f3f27683
bcuba <38017871+bcuba@users.noreply.github.com>
2020-01-21 12:34:19 -0800

    Update main.c

--- source/main.c
+++ source/main.c
@@ -10,101 +10,100 @@
 #include <avr/io.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
-#include <RIMS.h>
 #endif
 
-enum smP1_States {start, 01_P, 01_R, 10_P, 10_R} smP1_State;
+enum smP1_States {start, B0_P, B0_R, B1_P, B1_R} smP1_State;
 
 void smP1(){
-	switch(smP1_State) {/*Transitions*/
-		case start:
-			smP1_State = 01_P;
-			break;
-	
-		case 01_P:
-			if(A0){
-				smP1_State = 01_P;
-			}
-			else if(!A0){
-				smP1_State = 01_R;
-			}
-			else {
-				smP1_State = 01_P;
-			}
-			break;
-	
-		case 01_R:
-			if(!A0){
-				smP1_State = 01_R;
-			}
-			else if(A0){
-				smP1_State = 10_P;
-			}
-			else{
-				smP1_State = 01_R;
-			}
-			break;
-		
-		case 10_P:
-			if(A0) {
-				smP1_State = 10_P;
-			}
-			else if(!A0){
-				smP1_State = 10_R;
-			}
-			else{
-				smP1_State = 01_P;
-			}
-			break;
-	
-		case 10_R:
-			if(!A0){
-				smP1_State = 10_R;
-			}
-			else if(!A0){
-				smP1_State = 01_P;
-			}
-			else{
-				smP1_State = 10_R;
-			}
-			break;
-		default:
-			smP1_State = start;
-	}
-	switch(smP1_State){
-		case start:
-			break;
-	
-		case 10_P:
-			B0 = 0x01;
-			B1 = 0x00;
-			break;
+        unsigned char A0 = PINA;
+        switch(smP1_State) {/*Transitions*/
+                case start:
+                        smP1_State = B0_P;
+                        break;
 
-		case 10_R:		
-			break;
-		case 01_P;
-			B0 = 0x00;
-			B1 = 0x01;
-			break;
-		case 01_R;
-			break;
-		default:
-			B0 = 0x01;
-			B1 = 0x00;
-			break;
-	}
+                case B0_P:
+                        if(A0){
+                                smP1_State = B0_P;
+                        }
+                        else if(!A0){
+                                smP1_State = B0_R;
+                        }
+                        else {
+                                smP1_State = B0_P;
+                        }
+                        break;
+
+                case B0_R:
+                        if(!A0){
+                                smP1_State = B0_R;
+                        }
+                        else if(A0){
+                                smP1_State = B1_P;
+                        }
+                        else{
+                                smP1_State = B0_R;
+                        }
+                        break;
+
+                case B1_P:
+                        if(A0) {
+                                smP1_State = B1_P;
+                        }
+		else if(!A0){
+                                smP1_State = B1_R;
+                        }
+                        else{
+                                smP1_State = B1_P;
+                        }
+                        break;
+
+                case B1_R:
+                        if(!A0){
+                                smP1_State = B1_R;
+                        }
+                        else if(!A0){
+                                smP1_State = B0_P;
+                        }
+                        else{
+                                smP1_State = B1_R;
+                        }
+                        break;
+                default:
+                        smP1_State = start;
+        }
+        switch(smP1_State){
+                case start:
+                        break;
+
+                case B0_P:
+                        PORTB = 0x01;
+                        break;
+
+                case B0_R:
+                        break;
+                case B1_P:
+                        PORTB = 0x02;
+                        break;
+                case B1_R:
+                        break;
+                default:
+                        PORTB = 0x01;
+                        break;
+        }
 }
 
 int main(void) {
     /* Insert DDR and PORT initializations */
-	DDRA = 0x00;
-	PORTA = 0xFF;
-	DDRB = 0xFF;
-	PORTB = 0x00;
-	smP1_State = start;
+        DDRA = 0x00;
+        PORTA = 0xFF;
+        DDRB = 0xFF;
+        PORTB = 0x00;
+        smP1_State = start;
     /* Insert your solution below */
-    while (1) {
-	smP1();
+                   while (1) {
+        smP1();
     }
     return 1;
 }
+                                                                                                                                                                                                       102,1         91%
+
